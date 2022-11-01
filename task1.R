63d5b3f321c90e153a2199af51620d27f748a99e
Ant19on99zv96 <ant1999on96zv@gmail.com>
2022-11-01 04:19:15 +0300

    lab 1 done

---
+++ task1.R
@@ -0,0 +1,20 @@
+fix_number <-function (x){
+  loop_index_1 <- 0
+  for (y in x){
+    loop_index_1 <- loop_index_1+1
+    if (!is.na(as.numeric(sub(" ", "", y)))){
+      x[loop_index_1]<-as.numeric(as.character(sub(" ","",y)))
+      x<-as.numeric(x)
+    }
+  }
+  x
+}
+
+fix_data <-function (data){
+  data.frame(lapply(data,function(x) fix_number(x)))
+}
+
+setwd(paste0(getwd(), "/data"))
+data <- read.csv("lab1_e1.csv")
+data<-fix_data(data)
+str(data)
