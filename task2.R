4a75b2576a5325948522a50dc5520cbea86c7e95
Ant19on99zv96 <ant1999on96zv@gmail.com>
2022-12-31 21:25:14 +0300

    markdown fix

--- task2.R
+++
@@ -1,9 +0,0 @@
-get_id<-function (data){
-  df<-Reduce(function(df1, df2)  merge(df1, df2, by = "id"), data)
-  df$Avg_temp = rowMeans(df[, -1])
-  df<-df[c(1,ncol(df))]
-}
-
-data <- get(load("D:/studyr/data/lab1_e2.Rdata"))
-print(get_id(data))
-
